aa83954c939a899acd3e89a18a4768e785047e71
kazukitash <takahashi-k@daraf.co.jp>
2014-10-25 10:55:26 +0900

    require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,16 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap
+//= require bootstrap-sprockets
+//= require underscore
+//= require backbone
+//= require backbone-relational
+//= require tilt-jade/runtime
+//= require denko-keijiban
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
